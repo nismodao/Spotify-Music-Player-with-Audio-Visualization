27d0fd22dbb716708382322a34cf7b110c93869a
Nam Dao <nam.d.dao@gmail.com>
2016-07-27 10:33:27 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('underscore');
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(express.static(__dirname + '/public'));
+app.listen(PORT, function () {
+	console.log('Express listening on port ' + PORT + '!');
+});
+
