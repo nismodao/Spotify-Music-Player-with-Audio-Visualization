efefecd6c593c4c1d36a3a9e4d8ac8d0f222af05
Nam Dao <nam.d.dao@gmail.com>
2016-07-28 12:55:14 -0700

    modularize trackPlayer code

--- public/app.js
+++ public/app.js
@@ -1,5 +1,6 @@
 var audioObject = new Audio;
 var currentTrack = {};
+var url;
 
 function fetchTracks (albumId, callback) {
   $.ajax({
@@ -45,61 +46,65 @@ $('#albumResults').on('click', function (e) {
       if (audioObject) {
         audioObject.pause();
       }
-      fetchTracks(target.getAttribute('album-id'), function (data) {
-        trackList ={};    
-        data.tracks.items.forEach(function(item) {
-          trackList[item.name] = [item.preview_url, item.track_number];
-        });
-        document.getElementById("table").innerHTML="";
-        $("<tr><th>SONG</th><th>ARTIST</th><th>ALBUM</th><</tr>").appendTo($("#table"));
-        $.each(trackList, function(key,value) {
-          $("<tr id=" + "\"row\"" + "class=" + " " + ">" + "<td>" + "<div id=" + value[0] + " " + "class=" + " " + ">" + "<span class=" + " " + "\"glyphicon glyphicon-play-circle\"" + " " + ">" + "</span>" 
-          + "  " + value[1] + ". " + key + " " 
-          + " " + "</button>" + "</td>" + "<td>" + data.artists[0].name + "</td>" + " " + "<td>" + data.name + "</div>" +  "</td>" + "</tr>").appendTo($("#table"));
-        });
-        var albumLength = Object.keys(trackList).length;
-        var randomSong = Math.floor((Math.random()*albumLength));
-        audioObject = new Audio();
-        url =data.tracks.items[randomSong].preview_url;
-        $('#freq').slideDown('fast');
-        document.getElementById("alpha").style.display = "block";
-        window.scrollTo(0,500);
-        init();
-              
-        $("div[id^='http']").on('click', function(e) {
-          var inputButtonClicked = $(e.currentTarget);
-          if (currentTrack[inputButtonClicked.context.id]) {
-            audioObject.pause();
-            audioObject.addEventListener('pause', function () {
-              currentTrack[inputButtonClicked.context.id] = false;  
-            });
-          } else {
-            audioObject.pause();
-            audioObject = new Audio();
-            url = inputButtonClicked.attr('id');
-            init();
-            currentTrack[inputButtonClicked.context.id] = true;
-            inputButtonClicked.addClass('playing');
-            audioObject.addEventListener('pause', function () {
-              currentTrack[inputButtonClicked.context.id] = false;
-              inputButtonClicked.removeClass('playing');
-            });      
-          } 
-        });
-        currentTrack[target.outerHTML] = true;
-        target.classList.add('playing');
-        audioObject.addEventListener('ended', function () {
-          currentTrack[target.outerHTML] = false;
-          target.classList.remove('playing');
-        });
-        audioObject.addEventListener('pause', function () {
-          currentTrack[target.outerHTML] = false;
-          target.classList.remove('playing');
-        });
+      audioObject = new Audio();
+      currentTrack[target.outerHTML] = true;
+      target.classList.add('playing');
+      audioObject.addEventListener('ended', function () {
+        currentTrack[target.outerHTML] = false;
+        target.classList.remove('playing');
       });
+      audioObject.addEventListener('pause', function () {
+        currentTrack[target.outerHTML] = false;
+        target.classList.remove('playing');
+      });
+      trackListPlayer(target);
     }
   }
 });
+
+function trackListPlayer (target) {
+  fetchTracks(target.getAttribute('album-id'), function (data) {
+    var trackList = {};    
+    data.tracks.items.forEach(function(item) {
+      trackList[item.name] = [item.preview_url, item.track_number];
+    });
+    document.getElementById("table").innerHTML="";
+    $("<tr><th>SONG</th><th>ARTIST</th><th>ALBUM</th><</tr>").appendTo($("#table"));
+    $.each(trackList, function(key,value) {
+      $("<tr id=" + "\"row\"" + "class=" + " " + ">" + "<td>" + "<div id=" + value[0] + " " + "class=" + " " + ">" + "<span class=" + " " + "\"glyphicon glyphicon-play-circle\"" + " " + ">" + "</span>" 
+      + "  " + value[1] + ". " + key + " " 
+      + " " + "</button>" + "</td>" + "<td>" + data.artists[0].name + "</td>" + " " + "<td>" + data.name + "</div>" +  "</td>" + "</tr>").appendTo($("#table"));
+    });
+    var albumLength = Object.keys(trackList).length;
+    var randomSong = Math.floor((Math.random()*albumLength));
+    url =data.tracks.items[randomSong].preview_url;
+    $('#freq').slideDown('fast');
+    document.getElementById("alpha").style.display = "block";
+    window.scrollTo(0,500);
+    init();
+          
+    $("div[id^='http']").on('click', function(e) {
+      var inputButtonClicked = $(e.currentTarget);
+      if (currentTrack[inputButtonClicked.context.id]) {
+        audioObject.pause();
+        audioObject.addEventListener('pause', function () {
+          currentTrack[inputButtonClicked.context.id] = false;  
+        });
+      } else {
+        audioObject.pause();
+        audioObject = new Audio();
+        url = inputButtonClicked.attr('id');
+        init();
+        currentTrack[inputButtonClicked.context.id] = true;
+        inputButtonClicked.addClass('playing');
+        audioObject.addEventListener('pause', function () {
+          currentTrack[inputButtonClicked.context.id] = false;
+          inputButtonClicked.removeClass('playing');
+        });      
+      } 
+    });
+  });
+}
 
 Stop.addEventListener("click", function() {
   audioObject.pause();
