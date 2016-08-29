712684dd799bbb811ddd763355e14efcf9d3a63a
Nam Dao <nam.d.dao@gmail.com>
2016-08-28 17:44:43 -0700

    modified karma config

--- my.conf.js
+++ my.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-    'public/jquery.min.js', 'public/app.js', 'public/main.js', 'tests/*.js'
+    'public/*', 'tests/*.js'
     ],
 
 
